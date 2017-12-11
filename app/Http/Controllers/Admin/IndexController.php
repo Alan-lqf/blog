c2053877ee0c2265536a2d39b7ca1ff0421821dc
Alan-lqf <1993826783@qq.com>
2017-12-11 22:33:54 +0800

    index

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -11,8 +11,7 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $pdo = DB::connection()->getPdo();
-        dd($pdo);
+        return view('welcome');
 
     }
 }
