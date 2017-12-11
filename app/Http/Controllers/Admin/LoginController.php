02b66657fafad23d66c562e937e9e602e9d08f39
Alan-lqf <1993826783@qq.com>
2017-12-11 22:30:59 +0800

    登录模板引入

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends CommonController
+{
+    public function login()
+    {
+        return view('admin.login');
+
+    }
+}
